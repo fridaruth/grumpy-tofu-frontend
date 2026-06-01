4fe8be89ff8a62c09e779e18146c1366f3b69974
Frida <fridapihlstrom@gmail.com>
2026-06-01 16:34:16 +0200

    Sorterat upp menyn i automatiska kategoriblock, lagt till logga

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const API_URL = 'http://localhost:3000/api/menu';
 
 // hämta menyn från API
 async function fetchMenu() {
-  const menuContainer = document.getElementById('menu-container');
+  const loadingText = document.getElementById('menu-loading');
 
   try {
     const response = await fetch(API_URL);
@@ -18,38 +18,51 @@ async function fetchMenu() {
     const menuItems = await response.json();
 
     // rensa laddar menyn-texten
-    menuContainer.innerHTML = '';
+    loadingText.innerHTML = '';
 
     // om menyn är helt tom
     if (menuItems.length === 0) {
-      menuContainer.innerHTML = '<p class="loading">Menyn är tyvärr tom just nu. Kocken vilar..</p>';
+      loadingText.innerHTML = '<p class="loading">Menyn är tyvärr tom just nu. Kocken vilar..</p>';
+      loadingText.style.display = 'block';
       return;
     }
 
     // loopa igenom maträtter för att bygga HTML
     menuItems.forEach(item => {
-      const menuItemElement = document.createElement('div');
-      menuItemElement.classList.add('menu-item');
+      // gör om kategorinamnet
+      const itemCategory = item.category.toLowerCase();
 
-      menuItemElement.innerHTML = `
+      // hitta rätt kategoriblock
+      const categoryBlock = document.getElementById(`cat-${itemCategory}`);
+
+      if (categoryBlock) {
+        categoryBlock.style.display = 'block';
+
+        // hitta listan
+        const menuList = categoryBlock.querySelector('.menu-list')
+
+
+        const menuItemElement = document.createElement('div');
+        menuItemElement.classList.add('menu-item');
+
+        menuItemElement.innerHTML = `
       <div class="menu-item-header">
       <h3>${item.title}</h3>
       <div class="dots"></div>
       <span class="price">${item.price} kr</span>
       </div>
       <p class="description">${item.description}</p>
-      <span class="category">${item.category}</span>
       `;
 
-      menuContainer.appendChild(menuItemElement);
+        menuList.appendChild(menuItemElement);
+      }
     });
+
   } catch (error) {
     console.error('Fel vid hämtning av meny:', error);
-    menuContainer.innerHTML = `
-    <p class="loading" style="color: #d94b36;">
-  Hoppsan! Det gick inte att ansulta till köket. Se till att backend-servern är igång.
-  </p>
-  `;
+    loadingText.innerHTML = 'Hoppsan! Det gick inte att ansulta till köket. Se till att backend-servern är igång.';
+    loadingText.style.color = '#d94b35';
+    loadingText.style.display = 'block';
   }
 }
 
